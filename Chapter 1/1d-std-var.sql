49fae5107b2104c33156eeb627e435e3193e4894
Archan Lagoo <aklagoo@gmail.com>
2023-02-17 20:00:01 -0700

    Complete standard deviation and variance queries

---
+++ Chapter 1/1d-std-var.sql
@@ -0,0 +1,12 @@
+-- Calculate standard deviation and variance.
+select gender,
+    count(*),
+    avg(height_inches),
+    min(height_inches),
+    max(height_inches),
+    stddev_samp(height_inches),
+    stddev_pop(height_inches),
+    var_samp(height_inches),
+    var_pop(height_inches)
+from public.people_heights
+group by gender;
