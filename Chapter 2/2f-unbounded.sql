8d8d34b29c920af1c65f2db40c022c225df3c9e9
Archan Lagoo <aklagoo@gmail.com>
2023-02-19 19:13:27 -0700

    Completed 2f

--- Chapter 2/2f-unbounded.sql
+++ Chapter 2/2f-unbounded.sql
@@ -6,3 +6,15 @@ from sales.customers
 window win_company_frame as (order by company rows between unbounded preceding
 							 and unbounded following)
 order by company;
+
+select distinct customer_id,
+	first_value(order_date)
+		over(partition by customer_id
+			order by order_date
+			rows between unbounded preceding and unbounded following),
+	last_value(order_date)
+		over(partition by customer_id
+			order by order_date
+			rows between unbounded preceding and unbounded following)
+from sales.orders
+order by customer_id;
